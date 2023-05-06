67ac7afce67451b93bfa24afc373259e8e4c34a8
JOSEPH_CW_SHIH 施教旺 <s9443005@gmail.com>
2023-05-06 18:43:14 +0800

    Add files via upload

--- classicmodels/index.php
+++ classicmodels/index.php
@@ -47,19 +47,56 @@
                         <div class="card-body">
                             <h5 class="card-title"><i class="fs-2 bi-box-seam px-3 text-info"></i></i>產品</h5>
                             <?php
-                            $sql = "drop view if exists hotProduct;";
+                            $sql = "drop view if exists totalProductOrder;";
                             $result = $conn->query($sql); /* 若有VIEW先DROP */
-                            $sql = "create view hotProduct as select productCode, sum(quantityOrdered) as hotQuantity from orderdetails group by productCode;";
-                            $result = $conn->query($sql); /* 建立+VIEW先DROP */
-                            $sql = "select hotProduct.productCode, productName, max(hotQuantity) as maxQuantity from hotProduct, products;";
+                            $sql  = "create view if not exists totalProductOrder as ";
+                            $sql .= "select orderdetails.productCode, productName, sum(quantityOrdered) as quantityPerProduct, ";
+                            $sql .= "sum(quantityOrdered*priceEach) as salePerProduct ";
+                            $sql .= "from orderdetails, products ";
+                            $sql .= "where orderdetails.productCode=products.productCode ";
+                            $sql .= "group by productCode;";
+                            $result = $conn->query($sql); /* 建立VIEW */
+
+                            $sql = "select productCode, productName, max(quantityPerProduct) as MaxQuantityProduct from totalProductOrder;";
                             $result = $conn->query($sql);
                             $row    = $result->fetch_assoc();
-                            echo "<p class='card-text'>J站最熱銷商品：" . $row['productName'] . "</p>";
-                            echo "<p class='card-text'>己賣出：" . $row['maxQuantity'] . "</p>";
+                            echo "<p class='card-text'>J站最熱銷商品：" . $row['productCode'] . "</p>";
+                            echo "<p class='card-text'>" . $row['productName'] . "</p>";
+                            echo "<p class='card-text'>己售出：" . $row['MaxQuantityProduct'] . "</p>";
+
+                            $sql = "drop view if exists totalProductOrder;";
+                            $result = $conn->query($sql);
                             ?>
                             <a href="#" class="btn btn-primary">更多...</a>
                         </div>
                     </div>
+                    <div class="card m-3 p-3 shadow round-3" style="width: 18rem; background-image: linear-gradient(#ffffff, #ffffff, #88ffff)">
+                        <div class="card-body">
+                            <h5 class="card-title"><i class="fs-2 bi-calendar3 px-3 text-info"></i></i>產品</h5>
+                            <?php
+                            $sql = "drop view if exists totalProductOrder;";
+                            $result = $conn->query($sql); /* 若有VIEW先DROP */
+                            $sql  = "create view if not exists totalProductOrder as ";
+                            $sql .= "select orderdetails.productCode, productName, sum(quantityOrdered) as quantityPerProduct, ";
+                            $sql .= "sum(quantityOrdered*priceEach) as salePerProduct ";
+                            $sql .= "from orderdetails, products ";
+                            $sql .= "where orderdetails.productCode=products.productCode ";
+                            $sql .= "group by productCode;";
+                            $result = $conn->query($sql); /* 建立VIEW */
+
+                            $sql = "select productCode, productName, max(salePerProduct) as MaxSaleProduct from totalProductOrder;";
+                            $result = $conn->query($sql);
+                            $row    = $result->fetch_assoc();
+                            echo "<p class='card-text'>J站最熱銷商品：" . $row['productCode'] . "</p>";
+                            echo "<p class='card-text'>" . $row['productName'] . "</p>";
+                            echo "<p class='card-text'>銷貨收入：$" . $row['MaxSaleProduct'] . "</p>";
+
+                            $sql = "drop view if exists totalProductOrder;";
+                            $result = $conn->query($sql);
+                            ?>
+                            <a href="#" class="btn btn-primary">更多...</a>
+                        </div>
+                    </div>                    
                     <div class="card m-3 p-3 shadow round-3" style="width: 18rem; background-image: linear-gradient(#ffffff, #ffffff, #88ffff)">
                         <div class="card-body">
                             <h5 class="card-title"><i class="fs-2 bi-box-seam px-3 text-info"></i></i>產品</h5>
